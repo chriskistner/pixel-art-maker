282ea6ef8fb0f7a186185a7cf388d869408caf0f
Christopher Kistner <chris.kistner0@gmail.com>
2018-10-03 14:23:04 -0700

    Completed Base Challenge, begginging stretch goals

--- main.js
+++ main.js
@@ -13,27 +13,48 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     return artfield;
 }
-
 const renderField = createGrid(3101);
 mainBody.innerHTML = renderField.join('\n');
 //Color Selection
 const selectors = document.querySelectorAll(".selectors");
 const selected = document.querySelector(".selected");
-
-const change = function (event) {
-    if (!event.target.classList.contains("styleBlack")) {
-        event.target.classList.add('styleBlack');
-      } else {
-        event.target.classList.remove('styleBlack');
-      
-    }
-}
-/*const changeColor = function(event){
-    if (event.target.classList !== )
-}*/
-//Cell Interactivity
+const button = document.querySelector('button');
 const cells = document.querySelectorAll(".pixel");
-cells.forEach(cell => cell.addEventListener("click", change));
+const wheel = document.getElementById('colorWheel')
 
+let activeColor = '';
+let colorWheel = 'colorWheel';
+const update = function(event) {
+    if(!selected.classList.contains(event.target.classList.item(1))) {
+        selected.className = "";
+        selected.classList.add("selected",event.target.classList.item(1))
+        activeColor = selected.classList.item(1);
+    } 
+    else {selected.className = "selected"}
+}
+const change = function (event) {
+    if (!event.target.classList.contains(activeColor)) {
+        event.target.classList.add(activeColor);
+      }
+}
+
+const wheelUpdate = function () {
+    colorWheel.style.backgroundColor = wheel.value;
+    selected.classList.add('colorWheel')
+}
+const clear = function (event) {
+    return Array.from(cells).forEach( function(cell) {
+        if(cell.classList.contains("pixel")) {
+            cell.className = "";
+            cell.classList.add("pixel")
+        }
+    })
+}
+//Cell Interactivity
+cells.forEach(cell => cell.addEventListener("mousedown", change));
+cells.forEach(cell => cell.addEventListener("click",change))
+selectors.forEach(color => color.addEventListener("click", update))
+wheel.addEventListener('click', wheelUpdate);
+button.addEventListener("click", clear);
 });
 
